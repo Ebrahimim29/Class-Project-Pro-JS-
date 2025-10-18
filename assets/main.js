006558000506dd43836ada71bdb0962ee7c1567f
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-10-18 07:28:11 +0330

    part2

--- assets/main.js
+++ assets/main.js
@@ -58,3 +58,36 @@ const animals = [new Dog("Rex"), new Cat("Kitty"), new Animal("Lion")];
 
 // Polymorphism
 animals.forEach(animal => animal.speak());
+
+//-----------Getter/Setter------------
+class BankAccount{
+    #balance=0;
+    // _balance=0;
+
+    //Getter برای گرفتن مقدار موجودی- Get: با صدا زدن پراپرتی ازش استفاده می شود
+    get balance(){
+        return this.#balance;
+    }
+
+    //Setter برای تغییر مقدار موجودی- Set:تونستیم مقدار بالانس رو تغییر بدیم و ازش استفاده کنیم،فقط با صدازدن پراپرتی بالانس
+    set balance(amount){
+        console.log(amount); 
+        if(amount<0){
+            console.log("موجودی منفی مجاز نیست❌");            
+        }else{
+            this.#balance = amount;
+            console.log(`موجودی تغییر کرد✅: ${this.#balance}`);            
+        }
+    }
+}
+
+const account = new BankAccount();
+console.log(`موجودی فعلی: ${account.balance}`); //undefined: بدون پراپرتی Getter
+
+account.balance = 1000;
+console.log(`موجودی فعلی: ${account.balance}`);
+
+account.balance = -500;
+console.log(`موجودی فعلی: ${account.balance}`);
+
+// part249 5:37
